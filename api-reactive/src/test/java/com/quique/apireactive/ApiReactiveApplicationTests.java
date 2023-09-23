fdc3e97705aa6e1e45d1a377f2b148cccc768815
Quique <ncnr_quique@hotmail.com>
2023-09-23 00:05:55 -0500

    Mono, Flux, Observable..

---
+++ api-reactive/src/test/java/com/quique/apireactive/ApiReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quique.apireactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
