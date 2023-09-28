f4480062257b8040c36759106bbe27416e4947e5
Quique <ncnr_quique@hotmail.com>
2023-09-27 19:34:25 -0500

    creacion con api de gradle

---
+++ api-funtion/src/test/java/com/quique/apifuntion/ApiFuntionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quique.apifuntion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFuntionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
